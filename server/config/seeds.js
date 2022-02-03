b03c6528a8e1ce3eeb6a80547d93782e10150292
Adam Pham <thaihoangpham2008@gmail.com>
2022-02-03 13:30:02 +0800

    unfinished seeds

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -96,176 +96,182 @@ db.once('open', async () => {
             quantity: 1
         }, {
             name: 'Flat White',
-            description: 'Some things are just better together, like this single shot of espresso and steamed full cream milk.',
+            description: 'Like this single shot of espresso and steamed full cream milk.',
             image: './assets/hotCoffee/flatwhite.png',
             category: categories[1]._id,
             price: 4.50,
             quantity: 1
         }, {
-            name: 'Babyccino',
-            description: 'Perfect for the little one, this is a warm milk drink, frothed up with steam.',
-            image: './assets/hotCoffee/babycino.png',
+            name: 'Hot Chocolate',
+            description: 'Lovers of chocolate behold: steamed full cream milk made with chocolate powder.',
+            image: './assets/hotCoffee/hot-chocolate.png',
             category: categories[1]._id,
-            price: 3.50,
+            price: 4.00,
             quantity: 1
         }, {
-            name: 'Babyccino',
-            description: 'Perfect for the little one, this is a warm milk drink, frothed up with steam.',
-            image: './assets/hotCoffee/babycino.png',
+            name: 'Latté',
+            description: 'A classic mix of espresso, steamed full cream milk and a layer of on top.',
+            image: './assets/hotCoffee/latte.png',
             category: categories[1]._id,
-            price: 3.50,
+            price: 4.50,
             quantity: 1
         }, {
-            name: 'Babyccino',
-            description: 'Perfect for the little one, this is a warm milk drink, frothed up with steam.',
-            image: './assets/hotCoffee/babycino.png',
+            name: 'Long Black',
+            description: 'If you really need to wake yourself up, try a espresso poured over boiling water.',
+            image: './assets/hotCoffee/longblack.png',
             category: categories[1]._id,
-            price: 3.50,
+            price: 4.00,
             quantity: 1
         }, {
-            name: 'Babyccino',
-            description: 'Perfect for the little one, this is a warm milk drink, frothed up with steam.',
-            image: './assets/hotCoffee/babycino.png',
+            name: 'Macchiato',
+            description: 'A shot of espresso mixed with a dash of milk so you still get a strong coffee taste.',
+            image: './assets/hotCoffee/macchiato.png',
             category: categories[1]._id,
-            price: 3.50,
+            price: 4.50,
             quantity: 1
         }, {
-            name: 'Babyccino',
-            description: 'Perfect for the little one, this is a warm milk drink, frothed up with steam.',
-            image: './assets/hotCoffee/babycino.png',
+            name: 'Mocha',
+            description: 'A shot of espresso with steamed full cream milk and chocolate powder.',
+            image: './assets/hotCoffee/mocha.png',
             category: categories[1]._id,
-            price: 3.50,
-            quantity: 1
-        },
-        {
-            name: 'Raisin Toast with Butter',
-            description: 'Succulent raisins and aromatic spices, make this the toast of the town.',
-            image: './assets/toastie/raisin.png',
-            category: categories[4]._id,
             price: 4.50,
             quantity: 1
         },
         {
-            name: 'Raisin Toast with Butter',
-            description: 'Succulent raisins and aromatic spices, make this the toast of the town.',
-            image: './assets/toastie/raisin.png',
-            category: categories[4]._id,
-            price: 4.50,
+            name: 'Mixed Berry Smoothie',
+            description: 'Enjoy our refreshing, made from strawberry, raspberry, blueberry.',
+            image: './assets/drinks/berries-smoothie.png',
+            category: categories[3]._id,
+            price: 5.50,
             quantity: 1
         },
         {
-            name: 'Raisin Toast with Butter',
-            description: 'Succulent raisins and aromatic spices, make this the toast of the town.',
-            image: './assets/toastie/raisin.png',
-            category: categories[4]._id,
-            price: 4.50,
+            name: 'Classic Coke',
+            description: 'There is nothing like it with the famous, thirst-quenching taste.',
+            image: './assets/drinks/coke-classic.png',
+            category: categories[3]._id,
+            price: 3.50,
             quantity: 1
         },
         {
-            name: 'Raisin Toast with Butter',
-            description: 'Succulent raisins and aromatic spices, make this the toast of the town.',
-            image: './assets/toastie/raisin.png',
-            category: categories[4]._id,
-            price: 4.50,
+            name: 'Fanta',
+            description: 'Bright and bubbly, with that great orange taste.',
+            image: './assets/drinks/fanta.png',
+            category: categories[3]._id,
+            price: 3.50,
             quantity: 1
         },
         {
-            name: 'Raisin Toast with Butter',
-            description: 'Succulent raisins and aromatic spices, make this the toast of the town.',
-            image: './assets/toastie/raisin.png',
-            category: categories[4]._id,
+            name: 'Sprite',
+            description: 'It is refreshing, with a great lemon lime taste.',
+            image: './assets/drinks/sprite.png',
+            category: categories[3]._id,
+            price: 3.50,
+            quantity: 1
+        },
+        {
+            name: 'Tropical Smoothie',
+            description: 'Made with mango, pineapple and passionfruit plus no added sugar!',
+            image: './assets/drinks/tropical-smoothie.png',
+            category: categories[3]._id,
+            price: 5.50,
+            quantity: 1
+        },
+        {
+            name: 'Vanilla Thickshake',
+            description: 'Made with creamy, fresh milk, ingredients in a sweet vanilla flavour.',
+            image: './assets/drinks/vanilla-shake.png',
+            category: categories[3]._id,
+            price: 5.50,
+            quantity: 1
+        },
+        {
+            name: 'White Chocolate Iced Latté',
+            description: 'Espresso combined with milk and sugar syrup poured over ice with whipped cream.',
+            image: './assets/coldCoffee/chocolate-iced-latte.png',
+            category: categories[2]._id,
+            price: 5.00,
+            quantity: 1
+        },
+        {
+            name: 'Coffee Frappé',
+            description: 'A creamy Coffee Frappé, with irresistible whipped cream and indulgent drizzle.',
+            image: './assets/coldCoffee/coffee-frappe.png',
+            category: categories[2]._id,
+            price: 5.00,
+            quantity: 1
+        }, {
+            name: 'Iced Chocolate',
+            description: 'A delicious blend of full cream milk and chocolate syrup, plus whipped cream.',
+            image: './assets/coldCoffee/iced-chocolate.png',
+            category: categories[2]._id,
+            price: 5.00,
+            quantity: 1
+        }, {
+            name: 'Iced Latté',
+            description: 'A shot of smooth espresso combined with milk and sugar syrup poured over ice.',
+            image: './assets/coldCoffee/iced-latte.png',
+            category: categories[2]._id,
+            price: 5.00,
+            quantity: 1
+        }, {
+            name: 'Iced Long Black',
+            description: 'A shot of our smooth espresso with chilled water and ice.',
+            image: './assets/coldCoffee/iced-long-black.png',
+            category: categories[2]._id,
+            price: 5.00,
+            quantity: 1
+        }, {
+            name: 'Iced Mocha',
+            description: 'A shot of espresso combined with milk and chocolate syrup poured over ice.',
+            image: './assets/coldCoffee/iced-mocha.png',
+            category: categories[2]._id,
+            price: 5.00,
+            quantity: 1
+        },
+        {
+            name: 'Blueberry Muffin',
+            description: 'A baked blueberry muffin with blueberries mixed throughout.',
+            image: './assets/bakery/blueberry-muffin.png',
+            category: categories[5]._id,
+            price: 4.00,
+            quantity: 1
+        },
+        {
+            name: 'Chocolate Donut',
+            description: 'A donut decorated with chocolate icing and chocolate brown sprinkles.',
+            image: './assets/bakery/choc-donut.png',
+            category: categories[5]._id,
+            price: 4.00,
+            quantity: 1
+        },
+        {
+            name: 'Chocolate Raspberry Torte',
+            description: 'A dense almond based, topped with raspberry crumble and chocolate.',
+            image: './assets/bakery/choc-rasp-cheesecake.png',
+            category: categories[5]._id,
             price: 4.50,
             quantity: 1
         }, {
-            name: 'Raisin Toast with Butter',
-            description: 'Succulent raisins and aromatic spices, make this the toast of the town.',
-            image: './assets/toastie/raisin.png',
-            category: categories[4]._id,
-            price: 4.50,
+            name: 'Chocolate Macaron',
+            description: 'No one does macarons like us. One taste and you will see why.',
+            image: './assets/bakery/chocolate-macaron.png',
+            category: categories[5]._id,
+            price: 4.00,
             quantity: 1
         }, {
-            name: 'Raisin Toast with Butter',
-            description: 'Succulent raisins and aromatic spices, make this the toast of the town.',
-            image: './assets/toastie/raisin.png',
-            category: categories[4]._id,
-            price: 4.50,
+            name: 'Mud Muffin',
+            description: 'A baked choc muffin with choc chips and choc icing on top.',
+            image: './assets/bakery/mud-muffin.png',
+            category: categories[5]._id,
+            price: 4.00,
             quantity: 1
         }, {
-            name: 'Raisin Toast with Butter',
-            description: 'Succulent raisins and aromatic spices, make this the toast of the town.',
-            image: './assets/toastie/raisin.png',
-            category: categories[4]._id,
-            price: 4.50,
-            quantity: 1
-        }, {
-            name: 'Raisin Toast with Butter',
-            description: 'Succulent raisins and aromatic spices, make this the toast of the town.',
-            image: './assets/toastie/raisin.png',
-            category: categories[4]._id,
-            price: 4.50,
-            quantity: 1
-        }, {
-            name: 'Raisin Toast with Butter',
-            description: 'Succulent raisins and aromatic spices, make this the toast of the town.',
-            image: './assets/toastie/raisin.png',
-            category: categories[4]._id,
-            price: 4.50,
-            quantity: 1
-        }, {
-            name: 'Raisin Toast with Butter',
-            description: 'Succulent raisins and aromatic spices, make this the toast of the town.',
-            image: './assets/toastie/raisin.png',
-            category: categories[4]._id,
-            price: 4.50,
-            quantity: 1
-        }, {
-            name: 'Raisin Toast with Butter',
-            description: 'Succulent raisins and aromatic spices, make this the toast of the town.',
-            image: './assets/toastie/raisin.png',
-            category: categories[4]._id,
-            price: 4.50,
-            quantity: 1
-        }, {
-            name: 'Raisin Toast with Butter',
-            description: 'Succulent raisins and aromatic spices, make this the toast of the town.',
-            image: './assets/toastie/raisin.png',
-            category: categories[4]._id,
-            price: 4.50,
-            quantity: 1
-        }, {
-            name: 'Raisin Toast with Butter',
-            description: 'Succulent raisins and aromatic spices, make this the toast of the town.',
-            image: './assets/toastie/raisin.png',
-            category: categories[4]._id,
-            price: 4.50,
-            quantity: 1
-        }, {
-            name: 'Raisin Toast with Butter',
-            description: 'Succulent raisins and aromatic spices, make this the toast of the town.',
-            image: './assets/toastie/raisin.png',
-            category: categories[4]._id,
-            price: 4.50,
-            quantity: 1
-        }, {
-            name: 'Raisin Toast with Butter',
-            description: 'Succulent raisins and aromatic spices, make this the toast of the town.',
-            image: './assets/toastie/raisin.png',
-            category: categories[4]._id,
-            price: 4.50,
-            quantity: 1
-        }, {
-            name: 'Raisin Toast with Butter',
-            description: 'Succulent raisins and aromatic spices, make this the toast of the town.',
-            image: './assets/toastie/raisin.png',
-            category: categories[4]._id,
-            price: 4.50,
-            quantity: 1
-        }, {
-            name: 'Raisin Toast with Butter',
-            description: 'Succulent raisins and aromatic spices, make this the toast of the town.',
-            image: './assets/toastie/raisin.png',
-            category: categories[4]._id,
-            price: 4.50,
+            name: 'Salted Caramel Macaron',
+            description: 'For a quick flavour burst try this salted caramel treat.',
+            image: './assets/bakery/salted-caramel-macaron.png',
+            category: categories[5]._id,
+            price: 4.00,
             quantity: 1
         },
     ]);
