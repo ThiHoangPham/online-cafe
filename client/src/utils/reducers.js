571b325c625ccaa73a0831c376ec92c8148fa09b
Adam Pham <thaihoangpham2008@gmail.com>
2022-02-04 13:44:39 +0800

    unfinished reducers.js

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -9,3 +9,61 @@ import {
   CLEAR_CART
 } from "./actions";
 
+export const reducer = (state, action) => {
+    switch (action.type) {
+      case UPDATE_PRODUCTS:
+        return {
+          ...state,
+          products: [...action.products],
+        };
+  
+      case ADD_TO_CART:
+        return {
+          ...state,
+          cartOpen: true,
+          cart: [...state.cart, action.item],
+        };
+  
+      case ADD_MULTIPLE_TO_CART:
+        return {
+          ...state,
+          cart: [...state.cart, ...action.products],
+        };
+  
+      case REMOVE_FROM_CART:
+        let newState = state.cart.filter(item => {
+          return item._id !== action._id;
+        });
+  
+        return {
+          ...state,
+          cartOpen: newState.length > 0,
+          cart: newState
+        };
+  
+      case CLEAR_CART:
+        return {
+          ...state,
+          cartOpen: false,
+          cart: []
+        };
+  
+      case UPDATE_CATEGORIES:
+        return {
+          ...state,
+          categories: [...action.categories],
+        };
+  
+      case UPDATE_CURRENT_CATEGORY:
+        return {
+          ...state,
+          currentCategory: action.currentCategory
+        }
+  
+      default:
+        return state;
+    }
+  };
+
+
+  
