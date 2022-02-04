5e042b27fb205f2b9d5ec0d201b94b323da69af7
Adam Pham <thaihoangpham2008@gmail.com>
2022-02-04 13:42:35 +0800

    finished actions.js, working on reducers

---
+++ client/src/utils/reducers.js
@@ -0,0 +1,11 @@
+import { useReducer } from "react";
+import {
+  UPDATE_PRODUCTS,
+  ADD_TO_CART,
+  REMOVE_FROM_CART,
+  ADD_MULTIPLE_TO_CART,
+  UPDATE_CATEGORIES,
+  UPDATE_CURRENT_CATEGORY,
+  CLEAR_CART
+} from "./actions";
+
