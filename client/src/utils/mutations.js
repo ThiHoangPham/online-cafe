ba284c86b97e4ce48ec0f9ea48fc8432788a533e
Adam Pham <thaihoangpham2008@gmail.com>
2022-02-04 13:51:08 +0800

    imported mutation.js

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,45 @@
+mport gql from 'graphql-tag';
+
+export const LOGIN = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token 
+            user {
+                _id
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation addUser($firstName: String!, $lastName: String!, $email: String!, $password: String!) {
+        addUser(firstName: $firstName, lastName: $lastName, email: $email, password: $password) {
+            token
+            user {
+                _id
+            }
+        }
+    }
+`;
+
+export const ADD_ORDER = gql`
+    mutation addOrder($drinks: [ID]!) {
+        addOrder(drinks: $drinks) {
+            purchaseDate
+            drinks {
+                _id
+                name
+                description
+                price
+                category {
+                    name
+                  }
+                customize {
+                    size
+                    milk
+                    flavor
+                } 
+            }
+        }
+    }
+`;
