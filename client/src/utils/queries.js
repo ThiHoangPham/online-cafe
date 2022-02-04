d65a9f6bd020cf755cfdf92077631334d52ac86a
Adam Pham <thaihoangpham2008@gmail.com>
2022-02-04 13:37:57 +0800

    done/queries.js

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -52,5 +52,22 @@ export const QUERY_CATEGORIES = gql`
   }
 `;
 
-
+export const QUERY_USER = gql`
+  {
+    user {
+      firstName
+      lastName
+      orders {
+        _id
+        purchaseDate
+        products {
+          _id
+          name
+          description
+          price
+          image
+        }
+      }
+    }
+  }
 `;
