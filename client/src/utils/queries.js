b782f8d9a6c9656c3da72aa20bea2b27656b7c5b
Adam Pham <thaihoangpham2008@gmail.com>
2022-02-04 13:35:33 +0800

    unfinished queries.js

---
+++ client/src/utils/queries.js
@@ -0,0 +1,56 @@
+import gql from "graphql-tag";
+
+export const QUERY_PRODUCTS = gql`
+  query getProducts($category: ID) {
+    products(category: $category) {
+      _id
+      name
+      description
+      price
+      image
+      category {
+        _id
+      }
+      customize {
+        size
+        milk
+        flavor
+      }
+    }
+  }
+`;
+
+export const QUERY_CHECKOUT = gql`
+  query getCheckout($products: [ID]!) {
+    checkout(products: $products) {
+      session
+    }
+  }
+`;
+
+export const QUERY_ALL_PRODUCTS = gql`
+  {
+    products {
+      _id
+      name
+      description
+      price
+      image
+      category {
+        name
+      }
+    }
+  }
+`;
+
+export const QUERY_CATEGORIES = gql`
+  {
+    categories {
+      _id
+      name
+    }
+  }
+`;
+
+
+`;
