54f6d2ac7faa285358544a9ef7c8cfda6db057a2
Adam Pham <thaihoangpham2008@gmail.com>
2022-02-06 09:33:47 +0800

    unfinished pages/Login.js

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -10,7 +10,7 @@ const FooterStrap = () => {
         <MDBFooter className="font-small pt-4 mt-4">
             <div className="footer-copyright text-center py-3">
                 <MDBContainer fluid>
-                    &copy; {new Date().getFullYear()} Copyright: Thai Hoang Pham
+                    &copy; {new Date().getFullYear()} All Rights Reserved. Thai Hoang Pham
                 </MDBContainer>
             </div>
         </MDBFooter>
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -6,12 +6,71 @@ import Auth from "../utils/auth";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import {
-  Button,
-  Container,
-  Row,
-  Col,
-  Form,
-  Card
+    Button,
+    Container,
+    Row,
+    Col,
+    Form,
+    Card
 } from 'react-bootstrap';
 
+function LoginStrap(props) {
+    const [formState, setFormState] = useState({ email: '', password: '' })
+    const [login, { error }] = useMutation(LOGIN);
 
+    const handleFormSubmit = async event => {
+        event.preventDefault();
+        try {
+            console.log(formState.email)
+            console.log(formState.password)
+            const mutationResponse = await login({ variables: { email: formState.email, password: formState.password } })
+            const token = mutationResponse.data.login.token;
+            Auth.login(token);
+        } catch (e) {
+            console.log(e)
+        }
+    };
+
+    const handleChange = event => {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value
+        });
+    };
+
+    return (
+        <Container fluid="md">
+            <Row className="justify-content-md-center">
+                {/* <Col/> */}
+                <Col md="auto">
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Body>
+                            <Form onSubmit={handleFormSubmit} action='submit'>
+                                <Form.Group controlId="formEmail">
+                                    <Form.Label>Email address</Form.Label>
+                                    <Form.Control type="email" name="email" isRequired onChange={handleChange} />
+                                </Form.Group>
+
+                                <Form.Group controlId="formBasicPassword">
+                                    <Form.Label>Password</Form.Label>
+                                    <Form.Control type="password" name="password" isRequired onChange={handleChange} />
+                                </Form.Group>
+                                {
+                                    error ? <div>
+                                        <p className="error-text" >The provided credentials are incorrect</p>
+                                    </div> : null
+                                }
+                                <Button variant="dark" type="submit" block>
+                                    Login
+                                </Button>
+                            </Form>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default LoginStrap;
