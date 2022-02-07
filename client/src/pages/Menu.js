2d7cfedb13a19527e8d4ef3a938f0274c099f826
Adam Pham <thaihoangpham2008@gmail.com>
2022-02-07 16:27:01 +0800

    Menu.js

--- client/src/pages/Menu.js
+++ client/src/pages/Menu.js
@@ -6,9 +6,46 @@ import theme from '../theme/theme';
 
 import CategoryMenu from "../components/CategoryMenu";
 import ProductList from "../components/ProductList";
-//import ProductCard from '../components/ProductCard';
 import { QUERY_ALL_PRODUCTS } from '../utils/queries';
 import { useStoreContext } from "../utils/GlobalState";
 import { UPDATE_PRODUCTS } from "../utils/actions";
 import { idbPromise } from "../utils/helpers";
 
+const Menu = () => {
+    const [state, dispatch] = useStoreContext();
+    const { loading, data } = useQuery(QUERY_ALL_PRODUCTS);
+    console.log("state", state);
+    useEffect(() => {
+        if (data) {
+            dispatch({
+                type: UPDATE_PRODUCTS,
+                products: data.products
+            });
+            data.products.forEach((item) => {
+                idbPromise('products', 'put', item);
+            });
+        } else if (!loading) {
+            idbPromise('products', 'get').then((products) => {
+                dispatch({
+                    type: UPDATE_PRODUCTS,
+                    products: products
+                })
+            })
+        }
+    }, [loading, data, dispatch]);
+
+    return (
+        <ThemeProvider theme={theme}>
+            <Flex wrap="wrap" alignItems='center' justifyContent='center'>
+                <Box>
+                    <Box p={5} alignItems='center' justifyContent='center'>
+                        <CategoryMenu />
+                    </Box>
+                    <ProductList products={state.products} />
+                </Box>
+            </Flex>
+        </ThemeProvider>
+    );
+};
+
+export default Menu;
