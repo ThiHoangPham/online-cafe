b45278a7c47283dd30182e6fdd5b750292c6dd6f
Adam Pham <thaihoangpham2008@gmail.com>
2022-02-07 16:10:07 +0800

    stripe function

--- client/src/pages/MyOrder.js
+++ client/src/pages/MyOrder.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
-import { ThemeProvider, 
-    Stack, Flex, Box, Heading, Button } from '@chakra-ui/core';
-import theme  from '../theme/theme';
+import {
+    ThemeProvider,
+    Stack, Flex, Box, Heading, Button
+} from '@chakra-ui/core';
+import theme from '../theme/theme';
 import Auth from "../utils/auth";
 import CartItem from '../components/CartItem';
 import { useStoreContext } from "../utils/GlobalState.js";
@@ -16,3 +18,33 @@ import { Row, Col, Container } from 'reactstrap';
 
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+
+const MyOrder = () => {
+    const [state, dispatch] = useStoreContext();
+    const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
+    console.log(state);
+    useEffect(() => {
+        async function getCart() {
+            const cart = await idbPromise('cart', 'get');
+            dispatch({ type: ADD_MULTIPLE_TO_CART, drinks: [...cart] })
+        };
+        if (!state.cart.length) {
+            getCart();
+        }
+    }, [state.cart.length, dispatch]);
+
+    useEffect(() => {
+        if (data) {
+            stripePromise.then((res) => {
+                res.redirectToCheckout({ sessionId: data.checkout.session })
+            })
+        }
+    }, [data]);
+
+
+      );
+    };
+
+
+export default MyOrder;
