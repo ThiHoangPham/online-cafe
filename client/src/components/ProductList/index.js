02773ec242a195ac4b1f54ece39e514a9a3d0a75
Adam Pham <thaihoangpham2008@gmail.com>
2022-02-07 16:53:54 +0800

    unfinished components/ProductList/index.js

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,8 +1,64 @@
-import React from 'react';
-import { REMOVE_FROM_CART } from '../../utils/actions';
-import { useStoreContext } from '../../utils/GlobalState';
+import React, { useEffect } from "react";
+import ProductCard from "../ProductCard";
+import { useStoreContext } from "../../utils/GlobalState";
+import { UPDATE_PRODUCTS } from "../../utils/actions";
+import { useQuery } from '@apollo/react-hooks';
+import { QUERY_PRODUCTS } from "../../utils/queries";
 import { idbPromise } from "../../utils/helpers";
+import { ThemeProvider, Box, Grid, Flex} from '@chakra-ui/core';
+import theme from '../../theme/theme';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Card, CardTitle, CardText, CardBody, CardImg } from 'reactstrap';
-
+function ProductList({products}) {
+    const [state, dispatch] = useStoreContext()
+    const { currentCategory } = state;
+    const { loading, data } = useQuery(QUERY_PRODUCTS);
+  
+    useEffect(() => {
+      if(data) {
+        dispatch({
+             type: UPDATE_PRODUCTS,
+            products: data.products
+          });
+          data.products.forEach((product) => {
+            idbPromise('products', 'put', product);
+          });
+      } else if (!loading) {
+        idbPromise('products', 'get').then((products) => {
+          dispatch({
+            type: UPDATE_PRODUCTS,
+           products: products
+         });
+        });
+      }
+    }, [data, loading, dispatch]);
+  
+    function filterProducts() {
+      if (!currentCategory) {
+        return state.products;
+      }
+      return state.products.filter(product => product.category._id === currentCategory);
+    }
+  
+    return (
+      <ThemeProvider theme={theme}>
+           <Flex wrap='wrap' align='center' justify='center'>
+          <Box p={5}>
+              {state.products.length ? (
+                  <Grid templateColumns={["repeat(1, 1fr)","repeat(1, 1fr)","repeat(3, 1fr)","repeat(3, 1fr)"]} gap={3}>
+                      {filterProducts().map(product => (
+                          <ProductCard
+                          key= {product._id}
+                          item={product}
+                          />
+                      ))}
+                  </Grid>
+              ) : (
+                  <h3>You haven't added any products yet!</h3>
+              )}
+          </Box>
+          </Flex>
+      </ThemeProvider>
+    );
+  }
+  
+  export default ProductList;
