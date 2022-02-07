dfde26ddc2d830df5f66c8b82eec777e980b2a90
Adam Pham <thaihoangpham2008@gmail.com>
2022-02-07 17:08:14 +0800

    components/ProductCard/index.js has been done

--- client/src/components/ProductCard/index.js
+++ client/src/components/ProductCard/index.js
@@ -13,3 +13,99 @@ import {
 }
     from 'reactstrap';
 
+function ProductCard({ item }) {
+    const [show, setShow] = React.useState(false);
+    const [customize, setCustomize] = React.useState({ milk: "", size: "" })
+    const handleToggle = () => setShow(!show);
+    const [state, dispatch] = useStoreContext();
+    const { cart } = state;
+    const {
+        _id,
+    } = item;
+
+    const onChange = (e) => {
+        const { name, value } = e.target
+        setCustomize({ ...customize, [name]: value })
+    }
+
+    const addToCart = () => {
+        const itemInCart = cart.find((cartItem) => cartItem._id === _id);
+        item.customize = customize;
+        console.log("_______")
+        console.log(item);
+        dispatch({
+            type: ADD_TO_CART,
+            item: { ...item }
+        });
+        idbPromise('cart', 'put', { ...item });
+    }
+    console.log(cart);
+
+    return (
+        <div flexWrap="wrap" alignItems="center" justifyContent="center" maxW="500px" m="10">
+            <Card flexShrink="0" maxW="lg" bg="white.2" textAlign="center" flexBasis={['auto', '80%']}
+                width={[
+                    "100%", // base
+                    "50%", // 480px upwards
+                    "25%", // 768px upwards
+                    "15%", // 992px upwards
+                ]}>
+                <CardImg src={item.image} mt="10%" />
+                <CardBody p="4">
+                    <CardTitle mt="1" fontWeight="semibold" as="h3" lineHeight="tight" >
+                        {item.name} &nbsp;
+                        ${item.price}
+                    </CardTitle>
+                    <CardText as="span" fontSize="sm">
+                        {item.description}
+                    </CardText>
+                    <br></br>
+                    <Button
+                        onClick={handleToggle}
+                        borderRadius="8px"
+                        py="3"
+                        px="2"
+                        mt="4"
+                        lineHeight="1"
+                        size="md">
+                        Customize
+                    </Button>{'  '}
+                    <Collapse mt={4} isOpen={show}>
+                        <Form>
+                            <FormGroup>
+                                <Label for="exampleSelect">Select Size</Label>
+                                <Input type="select" name="size" id="exampleSelect" onChange={onChange}>
+                                    <option>Select</option>
+                                    <option>Small</option>
+                                    <option>Large (+$1.00)</option>
+                                </Input>
+                                <Label for="exampleSelect">Add Milk</Label>
+                                <Input type="select" name="milk" id="exampleSelect" onChange={onChange}>
+                                    <option>Select</option>
+                                    <option>No Milk</option>
+                                    <option>25% Milk</option>
+                                    <option>Almond Milk</option>
+                                </Input>
+                            </FormGroup>
+                        </Form>
+                        <Button id="PopoverFocus" type="button"
+                            borderRadius="8px"
+                            py="3"
+                            px="2"
+                            mt="2"
+                            lineHeight="1"
+                            size="md"
+                            onClick={addToCart}>
+                            Add to Cart
+                        </Button>
+                    </Collapse>
+                    <UncontrolledPopover trigger="focus" placement="bottom" target="PopoverFocus">
+                        <PopoverHeader>Item added to Cart!</PopoverHeader>
+                    </UncontrolledPopover>
+                </CardBody>
+            </Card>
+        </div>
+    );
+}
+
+export default ProductCard;
