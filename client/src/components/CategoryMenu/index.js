f98250843e176937ca2e4394aeaef5aff8ed427c
Adam Pham <thaihoangpham2008@gmail.com>
2022-02-07 17:23:04 +0800

    CategoryMenu/index.js

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -8,3 +8,59 @@ import { Box, Button } from '@chakra-ui/core';
 
 import './index.css';
 
+function CategoryMenu() {
+    const [state, dispatch] = useStoreContext();
+    const { categories } = state;
+    const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
+
+    useEffect(() => {
+        if (categoryData) {
+            dispatch({
+                type: UPDATE_CATEGORIES,
+                categories: categoryData.categories
+            });
+            categoryData.categories.forEach(category => {
+                idbPromise('categories', 'put', category);
+            });
+        } else if (!loading) {
+            idbPromise('categories', 'get').then(categories => {
+                dispatch({
+                    type: UPDATE_CATEGORIES,
+                    categories: categories
+                });
+            });
+        }
+    }, [categoryData, loading, dispatch]);
+
+    const handleClick = id => {
+        dispatch({
+            type: UPDATE_CURRENT_CATEGORY,
+            currentCategory: id
+        });
+    };
+
+    return (
+        <Box>
+            <h5>Categories.</h5>
+            {categories.map(item => (
+                <Button type="button"
+                    borderRadius="8px"
+                    py="3"
+                    px="2"
+                    mt="2"
+                    mx='2'
+                    lineHeight="1"
+                    size="md"
+                    key={item._id}
+                    onClick={() => {
+                        handleClick(item._id);
+                    }}
+                >
+                    {item.name}
+                </Button>
+            ))}
+        </Box>
+    );
+}
+
+export default CategoryMenu;
